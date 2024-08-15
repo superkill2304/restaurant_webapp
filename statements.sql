52eca07127ab440e4b48c1cafa2541b749d624cf
cristian <criistiansito14@gmail.com>
2024-08-14 21:03:58 -0500

    Creation of the main database

---
+++ statements.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS "users"(
+    "id" INTEGER,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    PRIMARY KEY("id")
+
+);
+
+CREATE TABLE IF NOT EXISTS "inventory"(
+
+    "id" INTEGER,
+    "user_id" INTEGER,
+    "category" TEXT NOT NULL CHECK("category" IN ("A","B","C","D","E","F","G")),
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL CHECK("price" > 0),
+    "quantity" REAL NOT NULL,
+    "supplier" TEXT,
+    "date"  CURRENT_TIMESTAMP, 
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id") 
+);
+
+CREATE TABLE IF NOT EXISTS "products"(
+
+    "id" INTEGER,
+    "user_id" INTEGER,
+    "name" TEXT NOT NULL UNIQUE,
+    "price" REAL, 
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "sells"(
+    "id" INTEGER,
+    "user_id" INTEGER,
+    "product_id" INTEGER
+    "quantity" INTEGER NOT NULL CHECK("quantity" > 0),
+    PRIMARY KEY("id")
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("product_id") REFERENCES "products"("id")
+)
